80934e6859fd263194b47e5e7e44ea2fff6ded38
ivan andres soto caro <dancerteddy9@gmail.com>
2020-02-09 17:42:30 -0500

    ultimo commit

---
+++ script.js
@@ -0,0 +1,64 @@
+
+const body = document.querySelector('#body');
+
+/* evento de agregarbotones */
+$(document).on("click", "#obtener", (e)=>{
+    e.preventDefault();
+    $("#botones").append(`
+    <button class="btn btn-primary">primary</button>
+    <button class="btn btn-secondary">secondary</button>
+    <button class="btn btn-success">success</button>
+    <button class="btn btn-danger">danger</button>
+    <button class="btn btn-warning">warning</button>
+    <button class="btn btn-info">info</button>
+    <button class="btn btn-light">light</button>  
+    `);
+   
+    $('#obtener').attr("disabled", true);
+});
+
+
+$(document).on("click", "#botones", (e)=>{
+    e.preventDefault();
+    let btn = e.target.className;
+    console.log(e.target.className);
+
+    switch(btn){
+
+        case 'btn btn-primary':
+           body.className = 'btn-primary';
+          break;
+
+        case 'btn btn-secondary':
+           body.className = 'btn-secondary';
+          break;
+
+        case 'btn btn-success':
+           body.className = 'btn-success';
+          break;
+
+        case 'btn btn-danger':
+            body.className = 'btn-danger';
+          break;
+
+        case 'btn btn-warning':
+            body.className = 'btn-warning';
+          break;
+
+        case 'btn btn-info':
+            body.className = 'btn-info';
+          break;
+
+        case 'btn btn-light':
+            body.className = 'btn-light';
+          break;
+          default:
+              alert('Debe presionar un boton')
+    }
+});
+        
+
+    
+
+
+
