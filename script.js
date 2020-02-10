b0cfe3b8a85ebd00971946708d58208706ca6876
ivan andres soto caro <dancerteddy9@gmail.com>
2020-02-10 10:14:48 -0500

    LocalStorage agregado

--- script.js
+++ script.js
@@ -21,36 +21,43 @@ $(document).on("click", "#obtener", (e)=>{
 $(document).on("click", "#botones", (e)=>{
     e.preventDefault();
     let btn = e.target.className;
-    console.log(e.target.className);
+    //console.log(e.target.className);
 
     switch(btn){
 
         case 'btn btn-primary':
            body.className = 'btn-primary';
+           localStorage.setItem('colorcito', 'btn-primary');
           break;
 
         case 'btn btn-secondary':
            body.className = 'btn-secondary';
+           localStorage.setItem('colorcito', 'btn-secondary');
           break;
 
         case 'btn btn-success':
            body.className = 'btn-success';
+           localStorage.setItem('colorcito', 'btn-success');
           break;
 
         case 'btn btn-danger':
             body.className = 'btn-danger';
+            localStorage.setItem('colorcito', 'btn-danger');
           break;
 
         case 'btn btn-warning':
             body.className = 'btn-warning';
+            localStorage.setItem('colorcito', 'btn-warning');
           break;
 
         case 'btn btn-info':
             body.className = 'btn-info';
+            localStorage.setItem('colorcito', 'btn-info');
           break;
 
         case 'btn btn-light':
             body.className = 'btn-light';
+            localStorage.setItem('colorcito', 'btn-light');
           break;
           default:
               alert('Debe presionar un boton')
@@ -59,6 +66,11 @@ $(document).on("click", "#botones", (e)=>{
         
 
     
-
+(()=>{
+  let color = localStorage.getItem('colorcito');
+  if(color){
+    body.className = color;
+  }
+})();
 
 
